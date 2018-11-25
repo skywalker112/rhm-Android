a97a671a3e429d310c36a79128dcc3ca87aa1992
skywalker112 <boguslaw.proszkowiec@gmail.com>
2018-11-25 18:51:58 +0100

    Initial commit

---
+++ Rhm Malinka/src/main/java/com/example/bpr/app2/AM2302.java
@@ -0,0 +1,89 @@
+package com.example.bpr.app2;
+
+import java.net.InetAddress;
+import java.net.DatagramSocket;
+import java.net.DatagramPacket;
+import java.io.*;
+import java.net.SocketTimeoutException;
+
+
+public class AM2302
+{
+    private static String ADDRESS = "89.66.60.110"; // athomelinux.ddns.net
+    private static int PORT = 50001;
+    private static String COMMAND = "MEASURE";
+    private static int SOCKET_TIMEOUT = 8000;
+
+    private static String[] printResponse(String text)
+    {
+        String PIPE = "|",	TEMP = "Temp=",	HUMIDITY = "Humidity=";
+        int pipe_position = text.indexOf(PIPE);
+        int temp_position = text.indexOf(TEMP);
+        int humidity_position = text.indexOf(HUMIDITY);
+        if(pipe_position == -1 || temp_position == -1 || humidity_position == -1)
+            return new String[] {"WRONG SERVER RESPONSE"};
+
+        //System.out.println("RESP : " + text);
+        String date = text.substring(0,pipe_position-1).trim();
+        String temp = text.substring(temp_position+TEMP.length(), humidity_position).trim();
+        String humidity = text.substring(humidity_position+HUMIDITY.length(), text.length()).trim();
+        return new String[] {date, temp, humidity};
+    }
+
+    private static String createResponse(String [] info)
+    {
+        if(info.length != 3)
+            return "FAILED";
+
+
+        return "Time of measure: " + info[0] + "\n" +
+                "Temperature: " + info[1] + "\n" +
+                "Humanidity: " + info[2] + "\n";
+    }
+
+    public static String infoUpdate() throws IOException
+    {
+        byte[] buffer = new byte[100];
+        String[] response;
+        InetAddress IPAddress= InetAddress.getByName(ADDRESS);
+/*
+        try {
+            IPAddress = InetAddress.getByName(ADDRESS);
+        } catch (Exception e) {
+            return "Cannot resolve the address!";
+        }
+
+*/
+        DatagramSocket socket = new DatagramSocket();
+        DatagramPacket sendPacket = new DatagramPacket(COMMAND.getBytes(), COMMAND.getBytes().length, IPAddress, PORT);
+        DatagramPacket receivePacket = new DatagramPacket(buffer, buffer.length);
+
+
+        try {
+
+            socket.send(sendPacket);
+
+        } catch (Exception e) {
+            return "Socket failed while sending request!";
+
+        } finally {
+
+            try {
+                socket.setSoTimeout(SOCKET_TIMEOUT);
+                socket.receive(receivePacket);
+
+            } catch(SocketTimeoutException e) {
+                return "Socket timeout while waiting for response!";
+
+            } catch (Exception e) {
+                return "Socket while waiting for response!";
+
+            } finally {
+                String serverResponse = new String(receivePacket.getData());
+                response = printResponse(serverResponse);
+            }
+        }
+        socket.close();
+        return createResponse(response);
+    }
+}
---
+++ Rhm Malinka/src/main/java/com/example/bpr/app2/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.bpr.app2;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    private static Boolean ready = true;
+    public void buttonUpdateClick(View v){
+
+        if(!ready) return;
+
+        final TextView textViewToChange = (TextView) findViewById(R.id.textView);
+
+        ready = false;
+        textViewToChange.setText("Run command");
+
+        try{
+            AM2302 var = new AM2302();
+            String result = var.infoUpdate();
+            textViewToChange.setText(result);
+        }
+        catch (Exception e) {
+            textViewToChange.setText("Exception occured in:  buttonUpdateClick function");
+            ready = true;
+        }
+        //textViewToChange.setText("Waiting...");
+        ready = true;
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
